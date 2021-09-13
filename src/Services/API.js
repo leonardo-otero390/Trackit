66083069d823c3b89b4cffa1593691da1a9cb1de
Leonardo <leonardo.otero390@gmail.com>
2021-09-12 22:29:14 -0400

    LogIn done

--- src/Services/API.js
+++ src/Services/API.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 
-const API_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up";
+const API_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth";
 
-function RequestSignUp(request,history,setLoading) {
+function RequestLogIn(request, history, setLoading) {
     setLoading(true);
-    axios.post(API_URL,request)
-    .then(()=>history.push("/"))
-    .catch(()=> {
-        alert("Não foi possível realizar o cadastro");
-        setLoading(false);
-    })
+    axios.post(API_URL + "/login", request)
+        .then(() => history.push("/hoje"))
+        .catch(() => {
+            alert("Não foi possível realizar o cadastro");
+            setLoading(false);
+        })
 }
-export { RequestSignUp}
+
+function RequestSignUp(request, history, setLoading) {
+    setLoading(true);
+    axios.post(API_URL + "/sign-up", request)
+        .then(() => history.push("/"))
+        .catch(() => {
+            alert("Não foi possível realizar o cadastro");
+            setLoading(false);
+        })
+}
+export { RequestSignUp, RequestLogIn }
--- src/pages/Home/LogIn.js
+++ src/pages/Home/LogIn.js
@@ -1,16 +1,38 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/Logo";
 import { Container, FormContainer, BlueButton } from "./style"
-import {TrackItInput} from "../../shared/styled-component/style"
+import { TrackItInput } from "../../shared/styled-component/style"
+import { useState } from "react";
+import Loader from "react-loader-spinner";
+import { RequestLogIn } from "../../Services/API";
+import { useHistory } from "react-router";
 
 export default function LogIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
+    const RequestBody = {
+        email: email,
+        password: password
+    }
+
     return (
         <Container>
             <Logo />
-            <FormContainer>
-                <TrackItInput type="text" placeholder="email" />
-                <TrackItInput type="password" placeholder="senha" />
-                <BlueButton>Entrar</BlueButton>
+            <FormContainer onSubmit={(e) => {
+                e.preventDefault();
+                RequestLogIn(RequestBody, history, setLoading);
+            }}>
+                <TrackItInput isLoading={loading} requiredtype="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+                <TrackItInput isLoading={loading} required type="password" placeholder="senha" value={password} onChange={e => setPassword(e.target.value)} />
+                <BlueButton type="submit">
+                    {loading ?
+                        <Loader type="ThreeDots" color="#fff" height={80} width={80} />
+                        :
+                        "Entrar"
+                    }
+                </BlueButton>
             </FormContainer>
             <h4>
                 <Link to="/cadastro">
